864fab3df8845547a549f39931778a45b8872418
Олександр <@DESKTOP-S660HFA>
2022-02-18 13:56:51 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Converter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Converter
+    {
+        public double USD { get; }
+        public double EUR { get; }
+        public double RUB { get; }
+
+        public Converter(double usd, double eur, double rub)
+        {
+            USD = usd;
+            EUR = eur;
+            RUB = rub;
+        }
+
+        public double ConvertToUsd(double value)
+        {
+            return value / USD;
+        }
+
+        public double ConvertToEur(double value)
+        {
+            return value / EUR;
+        }
+
+        public double ConvertToRub(double value)
+        {
+            return value / RUB;
+        }
+
+        public double ConvertFromUsd(double value)
+        {
+            return USD * value;
+        }
+
+        public double ConvertFromEur(double value)
+        {
+            return EUR * value;
+        }
+
+        public double ConvertFromRub(double value)
+        {
+            return RUB * value;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var converter = new Converter(28.49, 32.49, 0.38);
+
+            Console.WriteLine("Виберiть що потрiбно робити:");
+            Console.WriteLine("1: конвертувати у гривню");
+            Console.WriteLine("2: Конвертувати з гривнi");
+
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    ConvertTo(converter);
+                    break;
+                case 2:
+                    ConvertFrom(converter);
+                    break;
+            }
+
+            Console.WriteLine("Зроблено");
+            Console.ReadKey();
+        }
+
+        private static void ConvertTo(Converter Converter)
+        {
+            Console.WriteLine("Виберiть що потрiбно робити:");
+            Console.WriteLine("1: Конвертувавти з USD");
+            Console.WriteLine("2: Конвертувавти з EUR");
+            Console.WriteLine("3: Конвертувавти з RUB");
+
+            var option = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введiть сумму");
+
+            var input = double.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine(Converter.ConvertFromUsd(input));
+                    break;
+                case 2:
+                    Console.WriteLine(Converter.ConvertFromEur(input));
+                    break;
+                case 3:
+                    Console.WriteLine(Converter.ConvertFromRub(input));
+                    break;
+            }
+        }
+
+        private static void ConvertFrom(Converter Converter)
+        {
+            Console.WriteLine("Виберiть що потрiбно робити:");
+            Console.WriteLine("1: Конвертувавти в USD");
+            Console.WriteLine("2: Конвертувавти в EUR");
+            Console.WriteLine("3: Конвертувавти в RUB");
+
+            var option = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введiть сумму");
+
+            var input = double.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine(Converter.ConvertToUsd(input));
+                    break;
+                case 2:
+                    Console.WriteLine(Converter.ConvertToEur(input));
+                    break;
+                case 3:
+                    Console.WriteLine(Converter.ConvertToRub(input));
+                    break;
+            }
+        }
+    }
+}
